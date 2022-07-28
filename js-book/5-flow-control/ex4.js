3261044ad271564eebd20cced417ae625ebb9a91
Daniel Carter <90525500+bycarter@users.noreply.github.com>
2022-07-27 21:09:31 -0700

    5-ex4 push test

---
+++ js-book/5-flow-control/ex4.js
@@ -0,0 +1,4 @@
+// program will log Product2 to the console.  The program defines a function
+// with a switch on the parameter 'serial'. After, the function is called
+// with the argulent '113' which is the second case in the functions switch.
+//
