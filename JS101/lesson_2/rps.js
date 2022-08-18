5a034f2fe78a56bf9d86e690f06f13a384633080
Daniel Carter <90525500+bycarter@users.noreply.github.com>
2022-08-17 23:27:02 -0700

    added bonus features to rps and refactored

--- JS101/lesson_2/rps.js
+++ JS101/lesson_2/rps.js
@@ -1,48 +1,106 @@
 const rlSync = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors'];
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const VALID_CHOICES_SHORT = ['r', 'p', 's', 'l', 'k'];
+const WINNING_COMBOS = {
+  rock:     ['scissors', 'lizard'],
+  paper:    ['rock',     'spock'],
+  scissors: ['paper',    'lizard'],
+  lizard:   ['paper',    'spock'],
+  spock:    ['rock',     'scissors'],
+};
+
+while (true) {
+  prompt('Welcome to Rock Paper Scissors Lizard Spock');
+  prompt(''); // line break for aesthetics
+
+  let match = 1;
+  let userScore = 0;
+  let computerScore = 0;
+
+  while (true) {
+    prompt(`MATCH ${match}`);
+    prompt(`Choose One: ${VALID_CHOICES.join(', ')}`);
+    prompt("Pst... you may enter the first letter, or 'k' for spock!");
+    let choice = rlSync.question('~~> ');
+
+    while ((!VALID_CHOICES.includes(choice)) &&
+    (!VALID_CHOICES_SHORT.includes(choice))) {
+      prompt("That's not a valid choice");
+      prompt(`Choose One: ${VALID_CHOICES.join(', ')}`);
+      prompt("Pst... you may enter the first letter, or 'k' for spock!");
+      choice = rlSync.question('~~>');
+    }
+    choice = resolveUserInput(choice);
+
+    let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+    let computerChoice = VALID_CHOICES[randomIndex];
+
+    prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+    displayWinner(choice, computerChoice);
+
+    if (playerWins(choice, computerChoice)) {
+      userScore += 1;
+    } else if (playerWins(computerChoice, choice)) {
+      computerScore += 1;
+    }
+    displayScore(userScore, computerScore);
+
+    console.log(); // line break for aesthetics
+    match += 1;
+    if (match === 5) {
+      displayGrandWinner(userScore, computerScore);
+      break;
+    }
+  }
+
+  prompt('Do you want to play another match (y/n)? ');
+  let again = rlSync.question().toLowerCase();
+
+  while (again[0] !== 'y' && again[0] !== 'n') {
+    prompt('Please enter "y" or "n".');
+    again = rlSync.question('~~> ').toLowerCase();
+  }
+
+  if (again.toLowerCase() === 'n') break;
+  console.log(); // line break for aesthetics
+}
 
 function prompt(msg) {
   console.log(`~~> ${msg}`);
 }
-function displayWinner(choice, computerChoice) {
-prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+function resolveUserInput(choiceIn) {
+  let result = choiceIn;
 
-  if ((choice === 'rock' && computerChoice === 'scissors') ||
-      (choice === 'paper' && computerChoice === 'rock') ||
-      (choice === 'scissors' && computerChoice === 'paper')) {
-    prompt('You win!');
-  } else if ((choice === 'rock' && computerChoice === 'paper') ||
-             (choice === 'paper' && computerChoice === 'scissors') ||
-             (choice === 'scissors' && computerChoice === 'rock')) {
-    prompt('Computer wins!');
+  if (VALID_CHOICES_SHORT.includes(choiceIn)) {
+    for (let i = 0; i < VALID_CHOICES_SHORT.length; i++) {
+      if (choiceIn === VALID_CHOICES_SHORT[i]) {
+        result = VALID_CHOICES[i];
+      }
+    }
+  }
+  return result;
+}
+function playerWins(choice, computerChoice) {
+  return WINNING_COMBOS[choice].includes(computerChoice);
+}
+function displayWinner(choice, computerChoice) {
+  if (playerWins(choice, computerChoice)) {
+    prompt('You won!');
+  } else if (playerWins(computerChoice, choice)) {
+    prompt('Computer won!');
   } else {
     prompt("It's a tie!");
   }
 }
-
-while (true) {
-  prompt('Welcome to Rock Paper Scissors');
-  
-  prompt(`Choose One: ${VALID_CHOICES.join(', ')}`);
-  let choice = rlSync.question();
-  
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice");
-    prompt(`Choose One: ${VALID_CHOICES.join(', ')}`);
-    choice = rlSync.question();
+function displayScore(userIn, computerIn) {
+  prompt(`Your Score: ${userIn}, Computer Score: ${computerIn}`);
+}
+function displayGrandWinner(userIn, computerIn) {
+  if (userIn > computerIn) {
+    prompt('You are the GRAND WINNER!!');
+  } else if (computerIn > userIn) {
+    prompt('Computer is the GRAND WINNER!!');
+  } else {
+    prompt('Match is a TIE!!');
   }
-
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
-  
-  displayWinner(choice, computerChoice);
-
-  prompt('Do you want to play again (y/n)? ');
-  let again = rlSync.question().toLowerCase();
-  while (again[0] !== 'y' && again[0] !== 'n') {
-    prompt('Please enter "y" or "n".');
-    again = rlSync.question().toLowerCase();
-  }
-
-  if (again.toLowerCase() === 'n') break;
 }
