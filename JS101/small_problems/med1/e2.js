d147a10afb4156096ce4fee440bfba9f87074373
Daniel Carter <90525500+bycarter@users.noreply.github.com>
2022-09-14 21:44:03 -0700

    med problem 2 start

--- JS101/small_problems/med1/e2.js
+++ JS101/small_problems/med1/e2.js
@@ -1,1 +1,30 @@
+// input: number, number
+// output: number
+// data s: array
+// rules:
+//  rotate the nunber of digits in `num1` by the number in `num2`
+//  a rotation
+//    cuts a subString of digits of len `num2'
+//    removes the first char and appends to end
+//    concatenates subStr back onto initial string
+// algo:
+//  map number into array
+//  split array into subStrings where:
+//    `subArr2` is the last `num2` chars from the right of array
+//    `subArr1` is the rest of the chars
+//  declare `temp`;
+//  remove first char of `subArr2` and assign to `temp`
+//  concat `temp` to end of `subArr2`
+//  concat `subArr2` to end of `subArr1`
+//  return `subArr1`
 
+function rotateRightmostDigits(num1, num2) {
+
+
+// tests:
+rotateRightmostDigits(735291, 1);      // 735291
+rotateRightmostDigits(735291, 2);      // 735219
+rotateRightmostDigits(735291, 3);      // 735912
+rotateRightmostDigits(735291, 4);      // 732915
+rotateRightmostDigits(735291, 5);      // 752913
+rotateRightmostDigits(735291, 6);      // 352917
