ae67d858dcc9405df0233bbefac32537b9249457
Daniel Carter <90525500+bycarter@users.noreply.github.com>
2022-09-20 20:45:30 -0700

    twenty-one initial complete

--- JS101/lesson_6/twenty-one.js
+++ JS101/lesson_6/twenty-one.js
@@ -1,98 +1,111 @@
-// --> Gameplay
-// player and dealer are dealt
-//  > alternate cards, player can see only one of dealers cards
-// player is presented with three options
-//  > hit, player is dealt another card
-//    -> game calculates new total hand value
-//  > stay, player keeps current hand and waits for dealer turn
-// dealer turn
-//  > hit until value of hand is 17 or greater
-// determine if either busts
-// compare cards & declare winner
-
 const RL = require('readline-sync');
 const suitKey = {
-  'hearts': '♡', 'clubs': '♧', 'spades': '♤', 'diamonds': '♢'
+  hearts: '♡', clubs: '♧', spades: '♤', diamonds: '♢'
 };
-let fullDeck = { 
-  'hearts': [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
-  'clubs': [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
-  'spades': [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
-  'diamonds': [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+let fullDeck = {
+  hearts: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
+  clubs: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
+  spades: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'],
+  diamonds: [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
 };
-
-let deck = createDeck(fullDeck);
-let computerHand = [];
-let playerHand = [];
 
 while (true) {
+  let deck = createDeck(fullDeck);
+  let computerHand = [];
+  let playerHand = [];
+
   shuffle(deck);
   deal(deck, playerHand, computerHand);
   displayHandsSimple(playerHand, computerHand);
-  
+
+  current:
   while (true) {
-    let answer = RL.question('hit or stay? ');
-    if (answer === 'stay' || busted()) break;
+
+    while (true) {
+      console.log('');
+      let answer = RL.question('hit or stay? ');
+      if (answer === 'stay') break;
+
+      hitHand(deck, playerHand);
+      displayHandsSimple(playerHand, computerHand);
+
+      if (busted(playerHand)) {
+        console.log('BUST!');
+        break current;
+      }
+    }
+
+    while (true) {
+      console.log(computerHand);
+      if (total(computerHand) < 17) {
+        hitHand(deck, computerHand);
+        displayHandsSimple(playerHand, computerHand, false);
+      } else {
+        displayHandsSimple(playerHand, computerHand, false);
+        break;
+      }
+    }
+    console.log('');
+    console.log(displayWinner(playerHand, computerHand));
+
+    break;
   }
-  if (busted()) {
-    // end or ask to play again}
-  } else {
-    console.log('You chose to stay!');
-  }
-//
-break;
+  console.log('');
+  let again = RL.question('Play again? (y / n) ');
+  if (again !== 'y') break;
 }
 
 
-function displayHandsSimple(playerHandIn, computerHandIn) {
-  console.clear();
-  if (computerHandIn[0][1] === 10) {
-    console.log(`Dealer\'s hand: [${computerHandIn[0][1]} ${suitKey[computerHandIn[0][0]]}] [•  •]`);
+function displayWinner(playerHandIn, computerHandIn) {
+  let playerScore = total(playerHandIn);
+  let computerScore = total(computerHandIn);
+  if (playerScore > computerScore) {
+    return 'You won!!';
+  } else if (computerScore > playerScore) {
+    return 'Dealer won!!';
   } else {
-    console.log(`Dealer\'s hand: [${computerHandIn[0][1]}  ${suitKey[computerHandIn[0][0]]}] [•  •]`);
+    return 'It\'s a tie!';
+  }
+}
+function hitHand(deckIn, handIn) {
+  let deckPop = [];
+  deckPop = deckIn.pop();
+  handIn.push(deckPop);
+}
+function displayHandsSimple(playerHandIn, computerHandIn, hidden = true) {
+  console.clear();
+  if (hidden) {
+    if (computerHandIn[0][1] === 10) {
+      console.log(`Dealer's hand: [${computerHandIn[0][1]} ${suitKey[computerHandIn[0][0]]}] [•  •]`);
+    } else {
+      console.log(`Dealer's hand: [${computerHandIn[0][1]}  ${suitKey[computerHandIn[0][0]]}] [•  •]`);
+    }
+  } else {
+    console.log(`Dealer's hand: ${expandCards(computerHandIn)}`);
   }
   console.log('');
-  let playerHandStr = '';
-  playerHandIn.forEach(card => {
+  console.log(`Player's hand: ${expandCards(playerHandIn)}`);
+}
+function expandCards(handIn) {
+  let strOut = '';
+  handIn.forEach(card => {
     if (card[1] === 10) {
-      playerHandStr += '[' + card[1] + ' ' + suitKey[card[0]] + '] ';
+      strOut += ('[' + card[1] + ' ' + suitKey[card[0]] + '] ');
     } else {
-    playerHandStr += '[' + card[1] + '  ' + suitKey[card[0]] + '] ';
+      strOut += ('[' + card[1] + '  ' + suitKey[card[0]] + '] ');
     }
   });
-  console.log(`Player\'s Hand: ${playerHandStr}`);
+  return strOut;
 }
-/*
-function displayHands(playerHandIn, computerHandIn) {
-  console.log(`**************************************************`);
-  console.log(`               +---+----+`);
-  console.log(`               |${computerHandIn[0][1]}  |•  •|`);
-  console.log(`               |   |    |`);
-  console.log(`               |${suitKey[computerHandIn[0][0]]}  |•  •|`);
-  console.log(`               +---+----+`);
-  console.log('');
-  console.log('');
-  console.log('');
-  console.log('');
-  console.log(`            +----+ +----+`);
-  console.log(`            |${playerHandIn[0][1]}  ${suitKey[playerHandIn[0][0]]}| |${playerHandIn[1][1]}  ${suitKey[playerHandIn[1][0]]}|`);
-  console.log(`            |    | |    |`);
-  console.log(`            |${suitKey[playerHandIn[0][0]]}  ${playerHandIn[0][1]}| |${suitKey[playerHandIn[1][0]]}  ${playerHandIn[1][1]}|`);
-  console.log(`            +----+ +----+`);
-}*/
 function createDeck(deckIn) {
   let deckOut = [];
   for (let suit in deckIn) {
-    for (let num of deckIn[suit]) { 
+    for (let num of deckIn[suit]) {
       let card = [suit, num];
       deckOut.push(card);
     }
   }
   return deckOut;
-}
-function drawCard(deckIn) {
-  let card = deckIn.pop();
-  return card;
 }
 function deal(deckIn, handIn1, handIn2) {
   for (let num = 0; num < 2; num++) {
@@ -101,44 +114,33 @@ function deal(deckIn, handIn1, handIn2) {
   }
 }
 function shuffle(arr) {
-  for (let idx = arr.length -1; idx > 0; idx--) {
+  for (let idx = arr.length - 1; idx > 0; idx--) {
     let otherIdx = Math.floor(Math.random() * (idx + 1));
     [arr[idx], arr[otherIdx]] = [arr[otherIdx], arr[idx]];
   }
 }
+function total(handIn) {
+  let values = handIn.map(card => card[1]);
 
-function busted() {};
+  let sum = 0;
+  values.forEach(num => {
+    if (num === 'A') {
+      sum += 11;
+    } else if (['J', 'Q', 'K'].includes(num)) {
+      sum += 10;
+    } else {
+      sum += Number(num);
+    }
+  });
+  values.filter(num => num === 'A').forEach(_ => {
+    if (sum > 21) sum -= 10;
+  });
 
-/*
-
-+----+
-|♤  9|
-|    |
-|9  ♢|
-+----+
-+----+
-| ^ ̫^|
-|    |
-|^ ̫^ |
-+----+
-+----+
-|("< |
-|/ ) |
-| L  |
-+----+
-+----+
-|[O.o]
-|    |
-[O.o]|
-+----+
-+----+
-|•ㅅ•|
-|    |
-|•ㅅ•|
-+----+
-+----+
-|♤  ♢|
-|    |
-|♡  ♧|
-+----+
-*/
+  return sum;
+}
+function busted(handIn) {
+  if (total(handIn) <= 21) {
+    return false;
+  }
+  return true;
+}
