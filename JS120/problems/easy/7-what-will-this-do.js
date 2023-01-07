16093be6a861e9141d4ad19088d86c54afe832dd
Daniel Carter <90525500+bycarter@users.noreply.github.com>
2023-01-06 22:00:48 -0800

    more exercises

---
+++ JS120/problems/easy/7-what-will-this-do.js
@@ -0,0 +1,23 @@
+// what will the following code log?
+
+class Something {
+  constructor() {
+    this.data = "Hello";
+  }
+
+  dupData() {
+    return this.data + this.data;
+  }
+
+  static dupData() {
+    return "ByeBye";
+  }
+}
+
+let thing = new Something();
+console.log(Something.dupData());
+console.log(thing.dupData());
+
+/*
+This code will lot `'ByeBye'` and then `'HelloHello'` to the console.
+ */
