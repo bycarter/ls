938efee68bdee4d124ffa400a3ac29981bfa6d76
bycarter <90525500+bycarter@users.noreply.github.com>
2023-07-02 11:07:27 -0700

    Updated Study Prompts

--- JS130/Challenges/easy2/dna.js
+++ JS130/Challenges/easy2/dna.js
@@ -9,7 +9,34 @@ class DNA {
   constructor(seq1) {
     this.seq = seq1;
   }
-  
+  hammingDistance(seq2) {
+    /*
+    - if either seq len 0 === 0
+    algo:
+     - SET `shortSeq` = shorter seq
+     - SET `longSeq` = other seq
+     - SET `totMutations` = 0
+     - iterate over idx of `shortSeq`
+     -- IF char at idx of `shortSeq` !== `longSeq` -> += `totMutations`
+     - RETURN `totMutations`
+     */
+    let totMutations = 0;
+    let shortSeq;
+    let longSeq;
+    if (this.seq.length < seq2.length) {
+      shortSeq = this.seq;
+      longSeq = seq2;
+    } else {
+      shortSeq = seq2;
+      longSeq = this.seq;
+    }
+    for (let idx in shortSeq) {
+      if (shortSeq[idx] !== longSeq[idx]) {
+        totMutations += 1;
+      }
+    }
+    return totMutations;
+  }
 }
 
 module.exports = DNA;
