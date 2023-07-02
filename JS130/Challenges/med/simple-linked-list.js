a9f75ce20f44f565645ff3d27dece90cb059fcc0
bycarter <90525500+bycarter@users.noreply.github.com>
2023-07-02 10:39:46 -0700

    challenges

--- JS130/Challenges/med/simple-linked-list.js
+++ JS130/Challenges/med/simple-linked-list.js
@@ -1,30 +1,142 @@
 /*
-null <- elem1 <- elem2
-
+data structure:
+-element
+  [2|->{1}] [1|->null]
+  `tail` true if `ptr` -> null
+  `next` return `ptr`
  */
+'use strict';
+
 class Element {
-  constructor(num, prevElem = null) {
-    this.num = num;
-    this.prevElem = prevElem;
-    this.datum = prevElem ? prevElem.datum() : 1;
-    Element.elements.push(this);
+  constructor(datumIn, ptr = null) {
+    this.datumIn = datumIn;
+    this.ptr = ptr;
   }
-
-  static elements = [null]
-
   datum() {
-    return this.datum;
+    return this.datumIn;
   }
   isTail() {
-    return this.datum === 1;
+    return this.ptr === null;
   }
   next() {
-    return Element.elements[this.datum() - 1];
+    return this.ptr;
   }
 }
 
 class SimpleLinkedList {
+  constructor() {
+    this.currentSize = 0;
+    this.lList = [];
+  }
+  /*
+  rules:
+  - size starts 0
+  - isEmpty -> true if size 0
+  - push adds element of datum (arg)
+  -- inc `currentSize`
+  data structure:
+  - size
+   */
+  _incSize() {
+    this.currentSize += 1;
+  }
+  _decSize() {
+    this.currentSize -= 1;
+  }
+  size() {
+    return this.currentSize;
+  }
+  peek() {
+    if (this.size() === 0) {
+      return null;
+    } else {
+      return this.head().datum();
+    }
+  }
+  isEmpty() {
+    return this.currentSize === 0;
+  }
+  push(num) {
+    if (this.size() === 0) {
+      this.lList.push(new Element(num));
+    } else {
+      this.lList.push(new Element(num, this.lList[this.size() -1]));
+    }
+    this._incSize();
+  }
+  pop() {
 
-}
+    let last = this.lList.pop();
+    this._decSize();
+    return last.datum();
+  }
+  head() {
+    // LL) 1 -> null
+      // datum 1 -> head 1 && isTail === true
+    // LL) 2 -> 1 -> null
+      // datum 2 -> head 2 && isTail === false
+
+    // fromArray [1, 2]
+      // imp ???
+      // head -> 1
+      // head -> next -> 2
+      // Looks like reverse datum input (descending index)
+
+    // fromArray [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+      // peek -> 1
+      // head -> next^9 -> 10
+    return this.lList[this.size() - 1];
+  }
+
+  reverse() {
+    let reversedList = new SimpleLinkedList();
+
+    for (let elemNum = this.size() - 1; elemNum > 0; elemNum--) {
+      let currentElem = this.head();
+      if (elemNum === this.size() - 1) {
+        reversedList.push(currentElem.datum());
+      }
+      for (let traversals = 0; traversals <= elemNum - 1; traversals++) {
+        currentElem = currentElem.next();
+      }
+      reversedList.push(currentElem.datum());
+    }
+    //console.log(reversedList);
+    return reversedList;
+  }
+
+  static fromArray(arrIn) {
+    if (arrIn === null || arrIn.length === 0) {
+      return new SimpleLinkedList();
+    }
+    let lListOut = new SimpleLinkedList();
+    for (let idx = arrIn.length - 1; idx >= 0; idx--) {
+      lListOut.push(arrIn[idx]);
+    }
+    this.currentSize = arrIn.length;
+    return lListOut;
+  }
+  toArray() {
+    if (this.size() === 0) return [];
+
+    let arrayFromList = [];
+    let currentElem = this.head();
+
+    for (let listIdx = 0; listIdx < this.size(); listIdx++) {
+      arrayFromList.push(currentElem.datum());
+      currentElem = currentElem.next();
+    }
+    return arrayFromList;
+  }
+
+  }
+/*
+
+ */
 
 module.exports = {SimpleLinkedList, Element}
+
+let list = SimpleLinkedList.fromArray([1, 2, 3, 4]);
+console.log(list.lList);
+console.log(list.reverse().toArray());//.toArray());
+//console.log(list.reverse().peek());
--- JS130/sandbox.js
+++ JS130/sandbox.js
@@ -1,11 +1,25 @@
-console.log(foo); // logs `[Function: foo]`
-
-var foo = 6; // treated as a reassignment
-
-console.log(foo); // logs 6
-
-function foo() {
-  console.log();
+function makeCounter() {
+  return function() {
+    let counter = 0;
+    counter += 1;
+    return counter;
+  }
 }
 
-console.log(foo); //
+let incrementCounter = makeCounter();
+console.log(incrementCounter());
+console.log(incrementCounter());
+
+incrementCounter = makeCounter();
+console.log(incrementCounter());
+console.log(incrementCounter());
+
+/*
+The changes in this code will output `1`, `2`, `1`, `2` from lines 10, 11, 14,
+and 15 respectively.  Moving the `counter` variable declaration inside the
+return function encapsulates it, making it unavailable from the outer scope.
+Each returned function from `makeCounter` will have a unique `counter` variable,
+which is why the separate returned functions assigned to `incrementCounter` on
+lines 9 and 13 begin with `counter` initialized to `0`.
+
+ */
